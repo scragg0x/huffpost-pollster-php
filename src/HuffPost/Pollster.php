41d07e47b4dccc59432f1cc0f91fddd8d7d0e9a2
Matthew Scragg <scragg@gmail.com>
2013-06-18 18:08:30 -0500

    first

---
+++ src/HuffPost/Pollster.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace HuffPost;
+
+class Pollster {
+
+    public $client;
+
+    // Response format
+    public $format;
+
+    public $base_uri = 'http://elections.huffingtonpost.com/pollster/api';
+
+    public function __construct($format = 'json'){
+        $this->client = new Guzzle\Http\Client($this->base_uri);
+        $this->format = $format;
+    }
+
+    public function get($endpoint, $params = array()){
+        $endpoint .= "." . $this->format;
+        $req = $this->client->get($endpoint, $params)->send();
+        return ($this->format == 'json') ?  $req->json() : $req->body();
+    }
+
+    public function charts($state = null, $topic = null){
+        $endpoint = "/charts";
+        if ($state){
+            $endpoint .= "/" . $state;
+        }
+        else if ($topic){
+            $endpoint .= "/" . $state;
+        }
+        return $this->get($endpoint);
+    }
+
+    public function polls(){
+
+    }
+}
